7b51df0c7ac9e1fccc27549644a9fbdeefd35323
Muhammet Sarialioglu <muhammetsaralmkm@gmail.com>
2022-04-20 05:27:46 +0300

    Dosyalar Eklendi

---
+++ script.js
@@ -0,0 +1,23 @@
+let Name = prompt("Adınızı Giriniz");
+let myName = document.querySelector("#isim");
+
+myName.innerHTML = `${Name}!`;
+
+function sayac() {
+  let date = new Date().toLocaleString();
+  document.querySelector("#girissaati").innerHTML = date;
+}
+
+setInterval(sayac, 1000);
+
+let gun = new Date();
+let days = [
+  "PAZAR",
+  "PAZARTESİ",
+  "SALI",
+  "ÇARŞAMBA",
+  "PERŞEMBE",
+  "CUMA",
+  "CUMARTESİ",
+];
+document.querySelector("#girisgunu").innerHTML = days[gun.getDay()];
